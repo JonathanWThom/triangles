7376f31263f36bacb645de4c05aca2b9b00978f2
Jonathan Thom <jonathan@>
2016-10-20 15:33:39 -0700

    fix math, add css, separate front and back logic with a global function for math

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,24 @@
+//  Declare initial var vlaues
 var triangleType = "";
 var sideA = 0;
 var sideB = 0;
 var sideC = 0;
 
+// backend math
 var triangleTest = function (sideA, sideB, sideC) {
   if (!(sideA > 0) || !(sideB > 0) || !(sideC > 0)) {
     alert("Must be numbers greater than 0!");
   }
   else if ((sideA === sideB) && (sideB === sideC)) {
-  //	alert("equilateral");
    triangleType = "equilateral";
   }
   else if ((sideA !== sideB !== sideC) && ((sideA + sideB <= sideC)||(sideB + sideC <= sideA) || (sideA + sideC <= sideB) )){
-   //alert("not a triangle");
    triangleType = "not";
    }
   else if ((sideA === sideB) || (sideA === sideC) || (sideB === sideC)) {
-   //alert("iso");
    triangleType = "isosceles";
   }
   else if (sideA !== sideB !== sideC) {
-    //alert("scalene");
     triangleType = "scalene";
   }
   else {
@@ -28,7 +26,7 @@ var triangleTest = function (sideA, sideB, sideC) {
   }
   return triangleType;
 };
-
+// UI logic
 $(document).ready(function() {
   $("form").submit(function(event) {
     console.log(triangleType);
@@ -37,23 +35,7 @@ $(document).ready(function() {
     var sideC = parseInt($("input#sideC").val());
 
     triangleTest(sideA, sideB, sideC);
-    //alert('variables assigned');
-    console.log(triangleType);
-    if (triangleType === "equilateral") {
-      $("#equilateral").show();
-      console.log(triangleType + "something else");
-    }
-    else if (triangleType === "not") {
-      $("#not").show();
-      console.log(triangleType);
-    }
-    else if (triangleType === "isosceles") {
-      $("#isosceles").show();
-    }
-    else if (triangleType === "scalene") {
-      $("#scalene").show();
-    }
+    $("#output").text(triangleType);
     event.preventDefault();
   });
-  console.log(triangleType);
 });
