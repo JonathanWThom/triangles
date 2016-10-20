e17c115323e0af4503062f55fe0eb45ac15db7e6
Jonathan Thom <jonathan@>
2016-10-20 14:37:23 -0700

    Add html content, finish math

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,29 @@
 var triangleType = "";
  alert("functions being run");
 
- if ((sideA === sideB) && (sideB === sideC)) {
- 	alert("equilateral");
-   triangleType = "equilateral";
- } else if ((sideA === sideB) || (sideA === sideC) || (sideB === sideC)) {
-   alert("iso");
-   triangleType = "isosceles";
- } else if ((sideA !== sideB !== sideC) && ((sideA + sideB <= sideC)||(sideB + sideC <= sideA) || (sideA + sideC <= sideB) )){
-   alert("scalene");
-   triangleType = "scalene";
- } else {
-   alert("This is not a triangle!");
+if (!(sideA > 0) || !(sideB > 0) || !(sideC > 0)) {
+alert("non positive numbers");
+}
+else if ((sideA === sideB) && (sideB === sideC)) {
+	alert("equilateral");
+ triangleType = "equilateral";
+}
+else if ((sideA !== sideB !== sideC) && ((sideA + sideB <= sideC)||(sideB + sideC <= sideA) || (sideA + sideC <= sideB) )){
+ alert("not a triangle");
+ triangleType = "not"
+ }
+else if ((sideA === sideB) || (sideA === sideC) || (sideB === sideC)) {
+ alert("iso");
+ triangleType = "isosceles";
+}
+else if (sideA !== sideB !== sideC) {
+  alert("scalene");
+  triangleType = "scalene";
+}
+else {
+   alert("Form is incomplete");
  }
 
 $(document).ready(function() {
 
 });
-
-
-if ((sideA === sideB) && (sideB === sideC)) {
-  triangleType = "equilateral";
-  alert("equilateral");
-}
-
-else if ((sideA !== sideB !== sideC) && ((sideA + sideB <= sideC)||(sideB + sideC <= sideA) || (sideA + sideC <= sideB) )){
-  triangleType = "scalene";
-  console.log("scalene");
-} else {
-  alert("This is not a triangle!");
-  console.log("not");
-}
